e934a9d16aefa9548de837152edcab49e8c26eb5
your name <your email>
2022-05-26 20:51:25 +0200

    first

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,23 @@
+from random import randint, random, uniform
+
+
+Low = int(input('the smallest possible number:'))
+High = int(input('the largest possible number:'))
+NumToGuess = randint(Low,High)
+
+#command
+runG = True
+
+while runG == True:
+ x = int(input('witch number is it:'))
+ if x > NumToGuess:
+  print('lower')
+ elif x < NumToGuess:
+  print('higher')
+ elif x == NumToGuess:
+  print('you won')
+  runG = False    
+    
+    
+
+        
